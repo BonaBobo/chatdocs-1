d57ce73e3b5d495e9d1b1eeb67a6927a0806e508
mystique09 <knightkrypto7@gmail.com>
2023-03-11 14:02:30 +0800

    Add usecases for profile and chat in hooks.server.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,17 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { IProfileUsecase } from '$lib/server/domain/user';
+import type { IChatUsecase } from './lib/server/domain/llm';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			pb: import("pocketbase").default;
-			session: import("pocketbase").BaseModel | import("pocketbase").Admin | null;
+			pb: import('pocketbase').default;
+			session: import('pocketbase').BaseModel | import('pocketbase').Admin | null;
+			profileUsecase: IProfileUsecase;
+			chatUsecase: IChatUsecase;
 		}
 		// interface PageData {}
 		// interface Platform {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,37 @@
-import { BACKEND_URL } from "$env/static/private";
-import type { Handle } from "@sveltejs/kit";
-import PocketBase from "pocketbase";
+import { BACKEND_URL } from '$env/static/private';
+import { UserRepository } from '$lib/server/repository/user';
+import { ProfileUsecase } from '$lib/server/usecase/profile';
+import type { Handle } from '@sveltejs/kit';
+import PocketBase from 'pocketbase';
+import { ChatRepository } from './lib/server/repository/llm';
+import { ChatUsecase } from './lib/server/usecase/chat';
 
-export const handle: Handle = async ({event, resolve}) => {
-    event.locals.pb = new PocketBase(BACKEND_URL || "http://localhost:8090");
-    event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "");
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.pb = new PocketBase(BACKEND_URL || 'http://localhost:8090');
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
-    if (event.locals.pb.authStore.isValid) {
-        event.locals.session = event.locals.pb.authStore.model;
-    } else {
-        event.locals.session = null;
-        event.locals.pb.authStore.clear();
-    }
+	const chatRepository = new ChatRepository(event.locals.pb);
+	const chatUsecase = new ChatUsecase(chatRepository);
 
-    const response = await resolve(event);
-    response.headers.set(
-        "set-cookie",
-        event.locals.pb.authStore.exportToCookie({
-            secure: process.env.NODE_ENV === "production",
-        }),
-    );
-    return response;
+	const userRepository = new UserRepository(event.locals.pb);
+	const profileUsecase = new ProfileUsecase(userRepository);
+
+	event.locals.chatUsecase = chatUsecase;
+	event.locals.profileUsecase = profileUsecase;
+
+	if (event.locals.pb.authStore.isValid) {
+		event.locals.session = event.locals.pb.authStore.model;
+	} else {
+		event.locals.session = null;
+		event.locals.pb.authStore.clear();
+	}
+
+	const response = await resolve(event);
+	response.headers.set(
+		'set-cookie',
+		event.locals.pb.authStore.exportToCookie({
+			secure: process.env.NODE_ENV === 'production'
+		})
+	);
+	return response;
 };
