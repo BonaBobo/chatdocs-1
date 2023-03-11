2403f4d3696b416441ccf0a60a5398373eca5115
benjie <50514928+mystique09@users.noreply.github.com>
2023-03-11 04:09:02 +0000

    add: server hooks

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,10 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: import("pocketbase").default;
+			session: import("pocketbase").BaseModel | import("pocketbase").Admin | null;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-2xl text-purple-300">Welcome to SvelteKit</h1>
+<h1 class="text-2xl text-purple-300">Welcome to ChatDocs</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
