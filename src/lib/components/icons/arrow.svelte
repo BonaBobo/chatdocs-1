219391f8225dec69309bec62db4f8dae8f03e32b
mystique09 <knightkrypto7@gmail.com>
2023-03-16 15:44:03 +0800

    add: icons

---
+++ src/lib/components/icons/arrow.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let cl = 'w-6';
+</script>
+
+<svg viewBox="0 0 32 32" width="32" height="32" stroke="currentColor" fill="currentColor" class={cl}
+	><path
+		d="M16 2.7C8.6 2.7 2.7 7.5 2.7 13.3c0 3.9 2.5 7.4 6.6 9.3V29.3l7.2-5.3C23.6 23.8 29.3 19.1 29.3 13.3c0-5.9-6-10.7-13.3-10.6z"
+	/></svg
+>
